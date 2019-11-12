d8aabb41afc0b814d2e3b8ba3c83549bbd1d5dc5
anome-vj <anomehv@gmail.com>
2019-11-12 22:07:25 +0400

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+const reader= require("readline-sync");
 const util = require("./myUtils.js");
+const min= reader.question("Enter minimum number= ")
+const max=reader.question("Enter maximum number= ")
+let primes = [];
+for (i=min;i<=max;i++){
 
-console.log(util.isPrime(2));
+if(util.isPrime(i)){
+
+primes.push(util.isPrime(i));
+}
+}
+
+util.printPrimes(primes);
 
--- myUtils.js
+++ myUtils.js
@@ -1,19 +1,30 @@
-module.exports = {
-	isPrime: function(n) {
-		if(n <=1) {
+
+	
+	 function isPrime(i) {
+	
+		if(i <=1) {
 			return false;
 		}
 
 		let div = 2;
 
-		while(div <= Math.sqrt(n)) {
-			if(n % div === 0) {
+		while(div <= Math.sqrt(i)) {
+			if(i % div === 0) {
 				return false;
 			}
-
-			div++;
-		}
-
-		return true;
+			div++;	
+		}	 
+		 return i;
+		
 	}
+	
+  function printPrimes(primes){
+	   for(let i=0; i<primes.length;i++){
+		   console.log(primes[i]);
+	  }
+
+   }
+   module.exports = {
+	"printPrimes" :printPrimes,
+	"isPrime":isPrime,
 }
